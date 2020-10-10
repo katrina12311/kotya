69a44cd014e5416d21751d2ed27564d4da1847f6
katrina12311 <63542210+katrina12311@users.noreply.github.com>
2020-10-11 01:52:28 +0500

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
-<p> Задания: </p>
-<li><a href="lab1-5.php"><u>ЗАДАЧА №1-1</u></a></li>
-<li><a href="lab-2-10.php"><u>ЗАДАЧА №1-2</u></a></li>
-<li><a href="lab-2-11.php"><u>ЗАДАЧА №1-3</u></a></li>
-<?php
-
-?>
