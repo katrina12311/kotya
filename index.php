b61b9fadc975ba1ec9ef4c3ade9b6c9a8ce190e5
katrina12311 <63542210+katrina12311@users.noreply.github.com>
2020-10-26 15:32:54 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<p> Задания: </p>
+ЛР№1 </br>
+<ul id="nav"
+<li><a href="lab1-5.php"><u>ЗАДАЧА №1-1</u></a></li>
+<li><a href="lab-2-10.php"><u>ЗАДАЧА №1-2</u></a></li>
+<li><a href="lab-2-11.php"><u>ЗАДАЧА №1-3</u></a></li>
+</ul>
+ЛР№2 </br>
+<ul id="nav"
+<li><a href="lab2.4-1.php"><u>ЗАДАЧА №2.4-1</u></a></li>
+<li><a href="lab2.4-2.php"><u>ЗАДАЧА №2.4-2</u></a></li>
+<li><a href="lab2.5-1.php"><u>ЗАДАЧА №2.5-1</u></a></li>
+<li><a href="lab2.5-2.php"><u>ЗАДАЧА №2.5-2</u></a></li>
+<li><a href="lab2.6-1.php"><u>ЗАДАЧА №2.6-1</u></a></li>
+<li><a href="lab2.6-2.php"><u>ЗАДАЧА №2.6-2</u></a></li>
+</ul>
+ЛР№3 </br>
+<ul id="nav"
+<li><a href="s3-4.php"><u>ЗАДАЧА №3.4</u></a></li>
+<li><a href="s3-5.php"><u>ЗАДАЧА №3.5</u></a></li>
+<li><a href="s3-6.5.php"><u>ЗАДАЧА №3.6-5</u></a></li>
+<li><a href="s3-6.7.php"><u>ЗАДАЧА №3.6-7</u></a></li>
+<li><a href="s3-6.10.php"><u>ЗАДАЧА №3.6-10</u></a></li>
+</ul>
+<?php
+
+?>
