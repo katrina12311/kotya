f5172e9a8c2e9bd7cfb60b07248e05a6bc567fa3
katrina12311 <63542210+katrina12311@users.noreply.github.com>
2020-11-28 20:04:59 +0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,15 +1,13 @@
-      
 <style>
 		a {
 			
 			color:#FF6633;
-	
 		}
 </style>
 <ul id="nav">
-    <a href="main.php">Таблица "Предметы"</a><br><br>
-    <a href="smain.php">Таблица "Группа"</a><br><br>
-	<a href="tmain.php">Таблица "Расписание экзаменов"</a> 
+    <img src="https://icon-library.com/images/english-icon-png/english-icon-png-9.jpg" width="20" height="20"><a href="main.php">Таблица "Предметы"</a><br><br>
+    <img src="http://cdn.onlinewebfonts.com/svg/img_102793.png" width="20" height="20"><a href="smain.php">Таблица "Группа"</a><br><br>
+	<img src="https://www.seekpng.com/png/detail/802-8021123_from-october-17-through-28-all-eligible-u.png" width="20" height="20"><a href="tmain.php">Таблица "Расписание экзаменов"</a> 
 	<br><br><a>Нажмите </a><a style="color: red" href='pdf.php';>PDF</a><a>, чтобы скачать pdf файл. </a><br><br>
 	<a>Нажмите </a><a style="color: green" href='xls.php';>XML</a><a>, чтобы скачать xml файл. </a>
 </ul>
