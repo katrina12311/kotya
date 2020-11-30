dbe77a3eb68c67150ff6277014f0ec1477ab2db3
katrina12311 <63542210+katrina12311@users.noreply.github.com>
2020-11-30 12:14:14 +0500

    Update tupone.php
    Выпадающий список

--- tupone.php
+++ tupone.php
@@ -25,10 +25,34 @@
 	<h3>Редактирование</h3>
 	<form action="tuptwo.php" method="post">
 		<input type="hidden" name="id_rasp" value="<?=$prod['id_rasp']?>">
-		<p>ID Группы</p>
-		<input type="text" name="id_group" value="<?=$prod['id_group']?>">
-		<p>ID предмета</p>
-		<input type="text" name="id" value="<?=$prod['id']?>">
+		<?php
+		$result = mysqli_query($mysql, "SELECT id_group, name_group FROM groupa");
+		echo '<p style="color: #50c878">'."<br>Группа<br><br> <select name='id_group'>";
+        echo "<option selected value='$id_group'>$name_group</option>";
+		if ($result){
+                    // Для каждой строки из запроса
+                    while ($row = $result->fetch_array()){
+                        $id_group = $row['id_group'];
+                        $name_group = $row['name_group'];
+
+                        echo "<option value='$id_group'>$name_group</option>";
+                    }
+                }
+                echo "</select>";
+		$result = mysqli_query($mysql, "SELECT id, name FROM object");
+		echo '<p style="color: #50c878">'."Предмет<br><br> <select name='id'>";
+        echo "<option selected value='$id'>$name</option>";
+		if ($result){
+                    // Для каждой строки из запроса
+                    while ($row = $result->fetch_array()){
+                        $id = $row['id'];
+                        $name = $row['name'];
+
+                        echo "<option value='$id'>$name</option>";
+                    }
+                }
+                echo "</select>";
+		?>
 		<p>Дата консультации</p>
 		<input type="text" name="date_cons" value="<?=$prod['date_cons']?>">
 		<p>Дата экзамена</p>
